ac73cb9b639d1256eeb94557b833faa34f0bd7e6
mexavas <129474487+mexavas@users.noreply.github.com>
2023-06-01 01:13:09 +0900

    Update page.js

--- app/users/page.js
+++ app/users/page.js
@@ -1,13 +1,13 @@
 'use client' //컴포넌트 사용할 때 작성
 import Link from 'next/link';
 import './../materialize.css'
-import SideNav from './../sidenav';
+import SideNav from './../topnav';
 
 export default function SignUp() {
   return (
     <div>
-      <SideNav/>
-      <h1 className="center-align mexavas-text-white">OnMeet에 가입하시고 십구나🤗..개발 중...... 금방 나옵니다!</h1>
+      <TopNav/>
+      <h1 className="center-align mexavas-text-white">OnMeet회원가입 페이지 개발 중..!</h1>
     </div>
     
   )
