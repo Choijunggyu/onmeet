74ee765758e2cc5888a5df82303f7b1cd3099fea
mexavas <129474487+mexavas@users.noreply.github.com>
2023-06-01 01:58:34 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 'use client' //컴포넌트 사용할 때 작성
 import './materialize.css'
 import Link from 'next/link';
-import SideNav from './sidenav';
+import SideNav from './topnav';
  
 export default function Home() {
   return (
     <div>
-      <SideNav/>
+      <TopNav/>
       <h1 className="center-align mexavas-text-white">OnMeet에 오신 것을 환영합니🤗 홈페이지..개발 중....</h1>
     </div>
     
