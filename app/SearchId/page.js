f84e9a261318d8ed02e5b1e5af2be36488dacdb1
cjg <cjg721@naver.com>
2023-06-01 12:55:22 +0900

    react-select dependency 추가

--- app/SearchId/page.js
+++ app/SearchId/page.js
@@ -2,6 +2,7 @@ import '../materialize.css'
 import getdata from '../actions/getdata';
 import SearchId from './SearchId';
 import SideNav from '../sidenav';
+import TopNav from '../topnav';
 
 export default async function page(){
 
@@ -9,7 +10,7 @@ export default async function page(){
 
     return (
         <>
-        <SideNav/>
+        <TopNav/>
         <h2 className='mexavas-text-white center-align'>아이디 찾기</h2> 
         <div className='container'>
             <SearchId data = {data}/>
