9e56fc4a42d5036472a60c0fd937e35020beae35
greenishh <134532919+greenishh@users.noreply.github.com>
2023-06-06 18:29:08 +0900

    Update page.js

--- app/MyPage/page.js
+++ app/MyPage/page.js
@@ -1,13 +1,36 @@
+'use client'
+
+import { useState, useEffect } from 'react';
 import '../materialize.css'
 import TopNav from '../topnav';
+import Login from '../login/Login';
 import MyPage from './MyPage';
 
 export default function page() {
 
+  const [ isLogin, setIsLogin ] = useState();
+
+  useEffect(() => {
+      if (sessionStorage.getItem('id') === null) {
+          setIsLogin(false);
+      } else {
+          setIsLogin(true);
+      }
+  }, [isLogin]);    
+
     return (
-      <div>
-        <TopNav/>
-        <MyPage/>
-      </div>
+        <div>
+            <TopNav/>
+            {isLogin ?
+                <div>
+                    <MyPage/>
+                </div>
+              :
+                <div>
+                    <h5>로그인 후 이용해주세요.</h5>
+                    <button>로그인 하러 가기</button>
+                </div>
+            }
+        </div>
     )
 }
