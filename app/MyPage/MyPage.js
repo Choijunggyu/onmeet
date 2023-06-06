ee574b3894a1cdb0a2b7e457e393265dfa1f6354
greenishh <134532919+greenishh@users.noreply.github.com>
2023-06-06 19:46:24 +0900

    Update MyPage.js

--- app/MyPage/MyPage.js
+++ app/MyPage/MyPage.js
@@ -1,21 +1,12 @@
 'use client'
-import React, { useState, useRef } from 'react';
-import Select from 'react-select';
+import { useState, useEffect } from 'react';
+import Login from '../login/Login';
 
-let grade = [
-    { value:"1", label:"1" },
-    { value:"2", label:"2" },
-    { value:"3", label:"3" },
-    { value:"4", label:"4" },
-  ];
-
-export const MyPage = () => {
-
-    const [SelectValue, setSelectValue] = useState('');
-    const selectInputRef = useRef(null);
-
-        return (
-          <div className='row'>
+export default function MyPage() {
+    
+    return (
+        <div>
+            <div className='row'>
             <h3 className='mexavas-text-white center-align'>마이페이지</h3>
             <br></br>
 
@@ -26,45 +17,37 @@ export const MyPage = () => {
                 </div>
 
                 <div className='row'>
+                  <h5 className='mexavas-text-white col s3'>아이디</h5>
+                  <div className='card-panel col s5'>아이디</div>
+                </div>
+
+                <div className='row'>
                   <h5 className='mexavas-text-white col s3'>이메일</h5>
                   <div className='card-panel col s5'>이메일</div>
                 </div>
 
                 <div className='row'>
-                  <h5 className='mexavas-text-white col s3'>비밀번호</h5>
-                  <div className='card-panel col s5'>비밀번호</div>
+                  <h5 className='mexavas-text-white col s3'>학교</h5>
+                  <div className='card-panel col s5'>학교</div>
                 </div>
 
                 <div className='row'>
-
-                  <h5 className='mexavas-text-white col s3'>학적정보</h5>
-
-                  <div className='row'>
-                    <input className='mexavas-text-white col s3' type='text' name='school' id="school" placeholder='학교' />
-                  </div>
-
-                  <h5 className='col s3'>학적정보</h5>
-                  <div className='row'>
-                    <input className='mexavas-text-white col s3' type='text' name='major' id="major" placeholder='전공' />
-                    <Select className='center-align col s2'
-                      ref={selectInputRef}
-                      onChange={(e) => {
-                        if (e) {
-                          setSelectValue(e.value);
-                        } else {
-                          setSelectValue("");
-                        }
-                      }}
-                      options={grade}
-                      placeholder="학년"
-                    />
-                  </div>
-
+                  <h5 className='mexavas-text-white col s3'>전공</h5>
+                  <div className='card-panel col s5'>전공</div>
                 </div>
-                <button className="mexavas-blue waves-effect waves-light btn">회원 정보 수정</button>
+
+                <div className='row'>
+                  <h5 className='mexavas-text-white col s3'>학년</h5>
+                  <div className='card-panel col s5'>학년</div>
+                </div>
+
+                <div className='row'>
+                  <h5 className='mexavas-text-white col s3'>팀 정보</h5>
+                </div>
+
               </div>
-          </div>
-        );
-  }
-  
-  export default MyPage;
+
+            </div>
+        </div>
+    );
+}
