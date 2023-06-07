a5c8d5ccb90296bb63bb2fbe303bf05db93bb175
Mikrokosmos-EUN <117753343+Mikrokosmos-EUN@users.noreply.github.com>
2023-06-07 15:59:17 +0900

    로그인 성공

--- app/login/Login.js
+++ app/login/Login.js
@@ -6,26 +6,29 @@ export default function Login(props) {
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
 
-  const data = props.data; //가져온 props user db데이터  data.id 유저id data.password 유저비밀번호
-
-  useEffect(() => {
-    setId(data.id || ""); // 부모 컴포넌트로부터 전달된 id값
-    setPw(data.password || ""); //전달된 pw값
-  }, []);
-
   function comData(event) {
     event.preventDefault();
 
-    //로그인 처리
-    if (data.id === id) {
-      if (data.password === pw) {
-        window.alert(`로그인 성공`);
-      } else {
-        window.alert(`비밀번호가 일치하지 않습니다. 다시 입력해주세요`);
+    const data = props.data; //가져온 props user db데이터  data.id 유저id data.password 유저비밀번호
+
+    // 로그인 처리
+    let found = false;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id === id) {
+        found = true;
+        if (data[i].password === pw) {
+          window.alert(`로그인 성공`);
+        } else {
+          window.alert(`비밀번호가 일치하지 않습니다. 다시 입력해주세요`);
+        }
+        break;
       }
-    } else {
+    }
+
+    if (!found) {
       window.alert(`아이디가 일치하지 않습니다. 다시 입력해주세요.`);
     }
+      
 
     //로그인 성공한 경우 세션 스토리지에 정보 저장
     sessionStorage.setItem('id', id);
