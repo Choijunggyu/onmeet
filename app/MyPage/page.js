7388c61dbdf87375557e3e52df715050c6de8d10
greenishh <134532919+greenishh@users.noreply.github.com>
2023-06-09 11:35:47 +0900

    Update page.js

--- app/MyPage/page.js
+++ app/MyPage/page.js
@@ -1,38 +1,17 @@
-'use client'
-
-import { useState, useEffect } from 'react';
 import '../materialize.css'
+import GetUserData from '../actions/getdata'
 import TopNav from '../topnav';
-import Login from '../login/Login';
 import MyPage from './MyPage';
-import Link from 'next/link';
 
-export default function page() {
 
-  const [ isLogin, setIsLogin ] = useState(false);
+export default async function page() {
 
-  useEffect(() => {
-      if (sessionStorage.getItem('id') === null) {
-        
-      } else {
-          setIsLogin(true);
-      }
-  }, []);    
+    const data = await GetUserData()
 
     return (
         <div>
             <TopNav/>
-            {isLogin ?
-                <div>
-                    <MyPage/>
-                </div>
-              :
-                <div>
-                    <h5>로그인 후 이용해주세요.</h5>
-                    
-                    <Link  className=" waves-effect waves-light btn "href="./../login"><button>로그인 하러 가기</button></Link>  
-                </div>
-            }
+            <MyPage data = {data} />
         </div>
     )
 }
