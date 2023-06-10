f0b3436fe07a03e0903f988be9bfa170796a6414
pinkavas <131900196+pinkavas@users.noreply.github.com>
2023-06-11 02:03:50 +0900

    Update sideNav.js

--- app/sideNav.js
+++ app/sideNav.js
@@ -7,9 +7,10 @@ export default function SideNav(){
             <h3>메뉴</h3>
             <div className="divider"></div>
             <ul>
-                <Link className="mexavas-text-white mexavas-large-zize "href="/Team_list"><li>팀 게시판</li></Link><br/>
+                <Link className="mexavas-text-white mexavas-large-zize "href="/Teams"><li>전체 팀</li></Link><br/>
+                <Link className="mexavas-text-white mexavas-large-zize "href="/Teams"><li>팀 게시판</li></Link><br/>
                 <Link className="mexavas-text-white mexavas-large-zize "href="/Team_register"><li>팀 생성</li></Link><br/>
-                <Link className="mexavas-text-white mexavas-large-zize "href="/Team_manage"><li>팀 관리</li></Link><br/>
+                <Link className="mexavas-text-white mexavas-large-zize "href="/Team_manage"><li>내 팀 관리</li></Link><br/>
             </ul>
         </div>
     )
