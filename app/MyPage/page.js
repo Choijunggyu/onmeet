32ef6cedcd6025ed6aa78c8807e9e8486d208073
greenishh <134532919+greenishh@users.noreply.github.com>
2023-06-11 14:07:50 +0900

    Update page.js

--- app/MyPage/page.js
+++ app/MyPage/page.js
@@ -1,17 +1,18 @@
 import '../materialize.css'
-import {GetUserData} from '../actions/getdata'
+import { GetUserData, GetMemberData, GetTeamData } from '../actions/getdata'
 import TopNav from '../topnav';
 import MyPage from './MyPage';
-
 
 export default async function page() {
 
     const data = await GetUserData()
+    const memberdata = await GetMemberData()
+    const teamdata = await GetTeamData()
 
     return (
         <div>
             <TopNav/>
-            <MyPage data = {data} />
+            <MyPage data = {data} memberdata = {memberdata} teamdata = {teamdata} />
         </div>
     )
 }
