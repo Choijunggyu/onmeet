c1ebf5dea227adb25db8c12ace7377edff86d896
greenishh <134532919+greenishh@users.noreply.github.com>
2023-06-11 14:29:22 +0900

    팀 정보 출력 기능 추가

--- app/MyPage/MyPage.js
+++ app/MyPage/MyPage.js
@@ -5,14 +5,17 @@ import Link from 'next/link';
 export default function MyPage(props) {
 
     const [isLogin, setIsLogin] = useState(false);
-    const [info, setInfo] = useState('');  
+    const [info, setInfo] = useState('');
+   
+    const memberdata = props.memberdata;
+    const teamdata = props.teamdata;
 
     useEffect(() => {
-      if(sessionStorage.getItem('id') === null) {
+        if (sessionStorage.getItem('id') === null) {
 
-      } else {
-            setIsLogin(true);
-      }
+        } else {
+              setIsLogin(true);
+        }
     }, []);
 
     useEffect(() => {
@@ -26,52 +29,73 @@ export default function MyPage(props) {
     }, [props.data]);
 
     return (
-      <>
-          {isLogin ?
-            <div>
-                <h2 className='center-align'>My Page</h2>
-                <br></br>
-                <div className='row section'>
-                      <div className='col s4 offset-s4'>
-                            <div className='row'>
-                                <h6 className='col s4'>이름</h6>
-                                <div className='card-panel col s8'>{info.name}</div>
-                            </div>
-                            <div className='row'>
-                                <h6 className='col s4'>아이디</h6>
-                                <div className='card-panel col s8'>{info.id}</div>
-                            </div>
-                            <div className='row'>
-                                <h6 className='col s4'>이메일</h6>
-                                <div className='card-panel col s8'>{info.email}</div>
-                            </div>
-                            <div className='row'>
-                                <h6 className='col s4'>학교</h6>
-                                <div className='card-panel col s8'>{info.school}</div>
-                            </div>
-                            <div className='row'>
-                              <h6 className='col s4'>전공</h6>
-                              <div className='card-panel col s8'>{info.major}</div>
-                            </div>
-                            <div className='row'>
-                              <h6 className='col s4'>학년</h6>
-                              <div className='card-panel col s8'>{info.grade}</div>
-                            </div>
-                            <div className='row'>
-                              <h6 className='col s4'>팀 정보</h6>
+        <>
+              {isLogin ?
+                  <div>
+                      <h2 className='center-align'>My Page</h2>
+                      <br></br>
+                      <div className='row section'>
+                            <div className='col s4 offset-s4'>
+                                  <div className='row'>
+                                      <h6 className='col s4'>이름</h6>
+                                      <div className='card-panel col s8'>{info.name}</div>
+                                  </div>
+                                  <div className='row'>
+                                      <h6 className='col s4'>아이디</h6>
+                                      <div className='card-panel col s8'>{info.id}</div>
+                                  </div>
+                                  <div className='row'>
+                                      <h6 className='col s4'>이메일</h6>
+                                      <div className='card-panel col s8'>{info.email}</div>
+                                  </div>
+                                  <div className='row'>
+                                      <h6 className='col s4'>학교</h6>
+                                      <div className='card-panel col s8'>{info.school}</div>
+                                  </div>
+                                  <div className='row'>
+                                      <h6 className='col s4'>전공</h6>
+                                      <div className='card-panel col s8'>{info.major}</div>
+                                  </div>
+                                  <div className='row'>
+                                      <h6 className='col s4'>학년</h6>
+                                      <div className='card-panel col s8'>{info.grade}</div>
+                                  </div>
+                                  <div className='row'>
+                                      <h6 className='col s4'>팀 정보</h6>
+                                      <div className='card-panel col s8'>
+                                          {teamdata
+                                              .filter((team) => team.leaderId === sessionStorage.getItem('id'))
+                                                  .map((team) => (
+                                                      <div key={team.team_id}>
+                                                          {team.name}
+                                                      </div>
+                                          ))}
+                                          {memberdata
+                                              .filter((member) => member.uid === sessionStorage.getItem('id'))
+                                                  .map((member) => {
+                                                      const team = teamdata.find((team) => team.team_id === member.tid);
+                                                          if (team) {
+                                                              return (
+                                                                    <div key={team.team_id}>
+                                                                      {team.name}
+                                                                    </div>
+                                                              );
+                                                          }
+                                          })}
+                                      </div>
+                                  </div>
                             </div>
                       </div>
-                </div>
-            </div>
-            :
-            <div className='row section'>
-                <div className='container col s6 offset-s5'>
-                  <h4>로그인 후 이용해 주세요.</h4>
-                  <br></br>
-                  <Link className=" waves-effect waves-light btn "href="./../login">로그인 하러 가기</Link>  
-                </div>
-            </div>
-          }
+                  </div>
+                  :
+                  <div className='row section'>
+                      <div className='container col s6 offset-s5'>
+                          <h4>로그인 후 이용해 주세요.</h4>
+                          <br></br>
+                          <Link className=" waves-effect waves-light btn "href="./../login">로그인 하러 가기</Link>  
+                      </div>
+                  </div>
+              }
         </>
     );
 }
